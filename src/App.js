f2822de438761de861b5f59511377b407b63ce84
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-09-08 20:32:26 -0300

    feat: add open and download buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import pdfStr from "./utils/pdfStr";
+import base64toBlob from "./utils/base64toBlob";
+
+import "./App.css";
+
+function handleOpenPdf() {
+  const pdfBlob = base64toBlob(pdfStr, "application/pdf");
+  const url = URL.createObjectURL(pdfBlob);
+
+  var aEl = document.createElement("a");
+
+  aEl.setAttribute("href", url);
+
+  aEl.click();
+
+  URL.revokeObjectURL(url);
+}
+
+function handleDownloadPdf() {
+  const pdfBlob = base64toBlob(pdfStr, "application/pdf");
+  const url = URL.createObjectURL(pdfBlob);
+
+  var aEl = document.createElement("a");
+
+  aEl.setAttribute("href", url);
+
+  aEl.setAttribute("download", "blank.pdf");
+  aEl.setAttribute("target", "_blank");
+
+  aEl.click();
+
+  URL.revokeObjectURL(url);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={handleOpenPdf}>
+        Open PDF
+      </button>
+      <button type="button" onClick={handleDownloadPdf}>
+        Download PDF
+      </button>
     </div>
   );
 }
